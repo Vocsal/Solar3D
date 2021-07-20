6e58ce0bba413331fd697e57adfc5ceb019df4f9
张英贵 <zhangyinggui@wondershare.cn>
2021-07-20 20:41:00 +0800

    增加emissive纹理

--- src/js/planets/sun.ts
+++ src/js/planets/sun.ts
@@ -15,9 +15,10 @@ export default class Sun extends Planet {
         const sphere = new THREE.SphereGeometry(this.radius, 100, 100);
         const textureLoader = new THREE.TextureLoader();
         const meterial = new THREE.MeshPhongMaterial({
-            specular: 0x333333,
-            shininess: 15,
             map: textureLoader.load(SunMap),
+            emissive: 0xffffff,
+            emissiveMap: textureLoader.load(SunMap),
+            side: THREE.DoubleSide,
         })
         this.mesh = new THREE.Mesh(sphere, meterial);
         this.mesh.position.copy(this.initPosition);
