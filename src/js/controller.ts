3fa907662f01a468f32fdd1053bd71038cb7856d
Vocsal <vocsall@gmail.com>
2021-07-21 22:06:48 +0800

    预修改控制器

--- src/js/controller.ts
+++ src/js/controller.ts
@@ -9,3 +9,22 @@ export default class Controller {
         Controller.timeInterval = Controller.timeStamp - Controller.preTimeStamp;
     }    
 }
+
+// import * as THREE from 'three';
+
+// const Base = Date.now();
+// const Clock = new THREE.Clock();
+// Clock.start();
+
+// export default class Controller {
+//     static preTimeStamp: number  = Base + Clock.oldTime * 1000;
+//     static timeStamp: number = Base + Clock.startTime * 1000;
+//     static timeInterval: number = Clock.getDelta() * 1000;
+
+//     static update(): void {
+//         const delta = Clock.getDelta();
+//         Controller.preTimeStamp = Controller.timeStamp;
+//         Controller.timeStamp += delta * 1000;
+//         Controller.timeInterval = delta * 1000;
+//     }
+// }
