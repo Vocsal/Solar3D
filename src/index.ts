50ab0b55465679cccb70d17051496442f6a47a6b
张英贵 <zhangyinggui@wondershare.cn>
2022-02-25 17:51:57 +0800

    feat: 增加intro 控制器介绍

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,20 @@ import "src/css/solar.scss";
 import Solar from "src/js/solar";
 const solar = new Solar("body");
 solar.init();
+
+import introJs from 'intro.js';
+import 'intro.js/introjs.css';
+const intro = introJs();
+intro.setOptions({
+    showProgress: false,
+    showButtons: false,
+    showBullets: false,
+    steps: [{
+        element: ".dg.main.a",
+        title: "提示",
+        intro: "控制器可以对场景进行变换",
+        position: "bottom-left-aligned",
+    }],
+})
+
+intro.start();
