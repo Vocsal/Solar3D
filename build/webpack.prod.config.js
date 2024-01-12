2db5f643b0f92819f6a47fbea183017efc5a5551
Vocsal <vocsall@gmail.com>
2024-01-12 18:02:17 +0800

    chore: 修改构建配置

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,33 +1,35 @@
-const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.config.js');
-const webpack = require('webpack');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require("webpack-merge");
+const baseConfig = require("./webpack.config.js");
+const webpack = require("webpack");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 // 模块分析
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = merge(baseConfig, {
-    mode: 'production',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production'),
-        }),
-        new CleanWebpackPlugin(),
+const StaticDirName = "solar3d";
 
-        // 模块分析
-        // new BundleAnalyzerPlugin({
-        //     analyzerHost: "dev.solar3d.com",
-        //     analyzerPort: 7777,
-        // })
-    ],
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserPlugin(),
-        ]
-    }
-})
+module.exports = merge(baseConfig, {
+  mode: "production",
+  output: {
+    publicPath: `/${StaticDirName}/`,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    new CleanWebpackPlugin(),
+
+    // 模块分析
+    // new BundleAnalyzerPlugin({
+    //     analyzerHost: "dev.solar3d.com",
+    //     analyzerPort: 7777,
+    // })
+  ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+});
 
 // 代码分离，懒加载...
